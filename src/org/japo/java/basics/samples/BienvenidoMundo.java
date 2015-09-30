a4b8f55c41d2cc4329bf7cf6c4e91a35e9f47322
chrisxulo <cocodrilococota@gmail.com>
2015-09-30 12:49:35 +0200

    initial version

---
+++ src/org/japo/java/basics/samples/BienvenidoMundo.java
@@ -0,0 +1,13 @@
+package org.japo.java.basics.samples;
+
+public class BienvenidoMundo {
+
+    public static void main(String[] args) {
+        final double PI = 3.14;
+        double radio = 12.34;
+
+        System.out.println(PI * radio * radio);
+
+    }
+
+}
